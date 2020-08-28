a194b71546faf2363dc6ff5402297df6c31999d7
Frans Guelinckx <frans.guelinckx@gmail.com>
2020-08-28 09:40:24 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module com.demo.app {
+    requires spring.aop;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+}
---
+++ src/test/java/com/demo/app/ColApplicationTest.java
@@ -0,0 +1,12 @@
+package com.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {ColApplication.class})
+public class ColApplicationTest {
+
+    @Test
+    void loadContext() {
+    }
+}
