ed86beac4483f1fc5aa0c243ba726d2ddd5df2d6
Frans Guelinckx <frans.guelinckx@gmail.com>
2020-08-28 09:44:45 +0200

    add spring cloud kubernetes modules to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,7 @@ open module com.demo.app {
     requires spring.boot.autoconfigure;
     requires spring.context;
     requires spring.core;
+    requires spring.cloud.kubernetes.core;
+    requires spring.cloud.kubernetes.config;
+    requires spring.cloud.starter.kubernetes.config;
 }
